f8b8f767b53934eb7eeb052a03e0d379c90d01c2
gautam-cpp <gautamprajapat2509@gmail.com>
2025-04-08 03:23:27 +0530

    FIX: correct PageProps type for dynamic room route in Next.js App Routeer

--- frontend/src/app/(app)/[room]/page.tsx
+++ frontend/src/app/(app)/[room]/page.tsx
@@ -1,7 +1,7 @@
 import AppBar from "@/components/AppBar";
 import NormalUserHomePage from "@/components/NormalUserHomePage";
 
-export default async function Home({params}:{params:{room:string}}) {
+export default async function Home({ params }: { params: { room: string } }) {
   const {room} = await params;
   return (<div>
     <AppBar/>
