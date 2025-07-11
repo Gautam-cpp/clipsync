a294acbb207132e7e015b6eb256e3b8270d230e2
Gautam Prajapat <gautamprajapat2509@gmail.com>
2025-07-12 02:05:25 +0530

    Update roomUtils.ts

--- frontend/src/utils/roomUtils.ts
+++ frontend/src/utils/roomUtils.ts
@@ -1,2 +1,2 @@
-
-export const privateRoomIds = process.env.NEXT_PUBLIC_PRIVATE_ROOMID?.split(",") || [];
+export const privateRoomIds: string[] =
+  process.env.NEXT_PUBLIC_PRIVATE_ROOMID?.split(",").map(id => id.trim()) || [];
