fa048d77d6bf847f97389499bd3051e822adb0e5
gautam-cpp <gautamprajapat2509@gmail.com>
2025-07-24 18:54:10 +0530

    fix Linting Error by adding id type

--- frontend/src/utils/roomUtils.ts
+++ frontend/src/utils/roomUtils.ts
@@ -1,2 +1,4 @@
 export const privateRoomIds: string[] =
-  process.env.NEXT_PUBLIC_PRIVATE_ROOMID?.split(",").map((id:any) => id.trim()) || [];
+  process.env.NEXT_PUBLIC_PRIVATE_ROOMID
+    ? process.env.NEXT_PUBLIC_PRIVATE_ROOMID.split(",").map((id: string) => id.trim())
+    : [];
